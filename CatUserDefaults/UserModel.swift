4599819d220ea89ae0cfc1d48812988ad06a5890
Louis Tur <louis.tur@gmail.com>
2016-06-17 23:24:49 -0400

    updates user model a bit

--- CatUserDefaults/UserModel.swift
+++ CatUserDefaults/UserModel.swift
@@ -9,6 +9,11 @@
 import Foundation
 import Locksmith
 
+extension UserModel: Equatable {}
+func ==(lhs: UserModel, rhs: UserModel) -> Bool {
+  return lhs.userId == rhs.userId
+}
+
 internal class UserModel {
   
   private var userId: String = ""
@@ -16,18 +21,56 @@ internal class UserModel {
   internal private (set) var firstName: String = ""
   internal private (set) var lastName: String = ""
   internal private (set) var username: String = ""
-  internal private (set) var following: [UserModel]?
-  internal private (set) var followers: [UserModel]?
+  internal private (set) var following: [UserModel] = []
+  internal private (set) var followers: [UserModel] = []
   internal private (set) var email: String = ""
   
   private init() {
   }
   
-  convenience init(withJSON: [String : AnyObject]) {
+  convenience init?(withJSON json: [String : AnyObject]) {
     self.init()
     
+    guard let fName: String = json[" "] as? String,
+      let lName: String = json[" "] as? String,
+    let uName: String = json[" "] as? String,
+    let emailAdd: String = json[" "] as? String,
+    let id: String = json[" "] as? String,
+    let payment: String = json[" "] as? String else {
+      return
+    }
+    
+    self.firstName = fName
+    self.lastName = lName
+    self.userId = id
+    self.username = uName
+    self.email = emailAdd
+    self.paymentDetails = payment
+  }
+  
+  internal func addFollower(user: UserModel) -> [UserModel] {
+    self.followers.append(user)
+    return self.followers
+  }
+  
+  internal func removeFollower(user: UserModel) -> [UserModel] {
+    if let userIndex = self.followers.indexOf(user) {
+      self.followers.removeAtIndex(userIndex)
+    }
+    return self.followers
   }
   
   
+  internal func followUser(user: UserModel) -> [UserModel] {
+    self.following.append(user)
+    return self.following
+  }
+  
+  internal func unfollowUser(user: UserModel) -> [UserModel] {
+    if let userIndex = self.following.indexOf(user) {
+      self.following.removeAtIndex(userIndex)
+    }
+    return self.following
+  }
   
 }
